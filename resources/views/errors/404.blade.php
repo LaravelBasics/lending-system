97aad71a96f19807efd6ea092e7cd4e2a355eae2
abcdefghijkok <aaa111sssbbbb@gmail.com>
2025-03-19 19:54:14 +0900

    最初のコミット

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.errors')
+
+@section('title', __('404 Not Found'))
+@section('code', '404 Not Found')
+@section('message', __('ページが見つかりません'))
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.errors')
+
+@section('title', __('500 Internal Server Error'))
+@section('code', '500 Internal Server Error')
+@section('message', __('サーバー内部でエラーが発生しました'))
